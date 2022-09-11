6ab98bb5235e32675a8557a381069a527703459b
ayowilfred95 <ayowilfred1995@gmail.com>
2022-09-11 11:02:59 -0700

    C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ *  *
+ *   *main - Entry point
+ *    *
+ *     *Description: Using function puts
+ *      *
+ *       *Return: Always 0 (Success)
+ *        *
+ *         *
+ *          */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
